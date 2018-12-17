691acf21c3cd4a82916439948ce75dde5516bdbe
hfcorriez <hfcorriez@gmail.com>
2018-12-17 14:29:46 +0800

    remove test1

--- test1/home.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const request = require('request')
-
-exports.main = function (event) {
-  /* Here is your logic */
-  return { file: path.join(__dirname, '/index.html') }
-}
