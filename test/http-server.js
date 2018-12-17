b02ff1438f6dae48549abbc3ba9adfbeabc6b2e8
hfcorriez <hfcorriez@gmail.com>
2018-12-17 15:14:13 +0800

    support links

--- test/http-server.js
+++ test/http-server.js
@@ -1,6 +1,6 @@
 const Fly = require('../lib/fly')
 const fly = new Fly('lib/service')
-fly.add('example', 'test/example')
+fly.add('test/example', 'example')
 
 ~~(async () => {
   // console.log('functions', fly.list())
