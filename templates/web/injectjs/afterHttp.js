408ee941991cd19ec1a71b860d930b544cdfd0c1
hfcorriez <hfcorriez@gmail.com>
2019-11-26 21:01:33 +0800

    try inject to json

--- templates/web/injectjs/afterHttp.js
+++ templates/web/injectjs/afterHttp.js
@@ -2,6 +2,9 @@ module.exports = {
   main (event, ctx) {
     event.headers = event.headers || {}
     event.headers['x-powered-by'] = 'injectjs5'
+    if (typeof event.body === 'object') {
+      event.body.injectjs = true
+    }
     return event
   }
 }
