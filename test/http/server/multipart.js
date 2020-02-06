61bd7f60059f979a9456480c5fc7ef3d056e2308
hfcorriez <hfcorriez@gmail.com>
2020-02-06 11:07:27 +0800

    fix ignore match

--- test/http/server/multipart.js
+++ test/http/server/multipart.js
@@ -6,9 +6,6 @@ const debug = require('debug')('fly/srv/htt')
 const stat = promisify(fs.stat)
 
 module.exports = {
-
-  extends: './apiBase',
-
   async main (event) {
     const { body, files } = event
     const { f1, f2, f3 } = body
@@ -37,6 +34,7 @@ module.exports = {
     }
     return { files: fileList }
   },
+
   configHttp: {
     method: 'post',
     path: '/api/testMultipart',
