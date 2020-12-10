8503961cab07c7cd0c480292751d12dbcc88face
hfcorriez <hfcorriez@gmail.com>
2020-12-10 17:09:23 +0800

    basic hot reload

--- templates/project/home.js
+++ templates/project/home.js
@@ -3,7 +3,7 @@ const path = require('path')
 const Func = {
   main (event, { fly }) {
     // console.log(ctx)
-    fly.info('home index 2')
+    fly.info('home index 7')
     return { file: path.join(__dirname, '/index.html') }
   },
 
