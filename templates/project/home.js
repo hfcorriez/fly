e70f2b6186118232a809e7a029b6059ea921ec8b
hfcorriez <hfcorriez@gmail.com>
2020-12-10 16:46:19 +0800

    fix log

--- templates/project/home.js
+++ templates/project/home.js
@@ -3,7 +3,7 @@ const path = require('path')
 const Func = {
   main (event, { fly }) {
     // console.log(ctx)
-    fly.info('home index')
+    fly.info('home index 2')
     return { file: path.join(__dirname, '/index.html') }
   },
 
