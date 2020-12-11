86aaaf08aa5472a2f9d12780bcc1d9e3079c9b64
hfcorriez <hfcorriez@gmail.com>
2020-12-11 09:27:24 +0800

    add compile

---
+++ functions/commands/compile.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async main () {
+    console.log('compile ok')
+  },
+
+  configCommand: {
+    _: 'compile',
+    descriptions: {
+      _: 'Compile function cache'
+    }
+  }
+}
