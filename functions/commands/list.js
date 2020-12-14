0d07cb275fda41290130b9315726aef0ca14d3b7
hfcorriez <hfcorriez@gmail.com>
2020-12-15 00:44:49 +0800

    improve list function

--- functions/commands/list.js
+++ functions/commands/list.js
@@ -2,13 +2,18 @@ const Table = require('cli-table3')
 
 module.exports = {
   main (event, { fly }) {
-    const functions = fly.find(event.params.type)
+    const { type } = event.params
+    const { system } = event.args
+    const functions = fly.find(type)
     const table = new Table({
       head: ['Name', 'Events', 'File'],
       chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' }
     })
 
+    console.log('system', system)
+
     functions.forEach(fn => {
+      if (!system && fn.prefix === '$') return
       table.push([fn.name, Object.keys(fn.events).join(',') || '-', fn.path])
     })
 
@@ -17,8 +22,15 @@ module.exports = {
 
   configCommand: {
     _: 'list [type]',
+    args: {
+      '--system': Boolean
+    },
+    alias: {
+      '--system': '-s'
+    },
     descriptions: {
-      _: 'List functions'
+      _: 'List functions',
+      '--system': 'Show system functions'
     }
   }
 }
