1d6c428b9709d448ddf25e7e00937f5614f11939
hfcorriez <hfcorriez@gmail.com>
2020-12-15 00:57:52 +0800

    get -> show, remove unused retry

--- functions/commands/list.js
+++ functions/commands/list.js
@@ -2,11 +2,11 @@ const Table = require('cli-table3')
 
 module.exports = {
   main (event, { fly }) {
-    const { type } = event.params
+    const { filter } = event.params
     const { system } = event.args
-    const functions = fly.find(type)
+    const functions = fly.find()
     const table = new Table({
-      head: ['Name', 'Events', 'File'],
+      head: ['Name', 'Props', 'Events', 'File'],
       chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' }
     })
 
@@ -14,14 +14,18 @@ module.exports = {
 
     functions.forEach(fn => {
       if (!system && fn.prefix === '$') return
-      table.push([fn.name, Object.keys(fn.events).join(',') || '-', fn.path])
+      if (filter) {
+        const fullStr = [fn.namem, Object.keys(fn.events).join(','), fn.path].join('')
+        if (!fullStr.includes(filter)) return
+      }
+      table.push([fn.name, Object.keys(fn.props || {}).join(','), Object.keys(fn.events).join(',') || '-', fn.path])
     })
 
     console.log(table.toString())
   },
 
   configCommand: {
-    _: 'list [type]',
+    _: 'list [filter]',
     args: {
       '--system': Boolean
     },
