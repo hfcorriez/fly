e8c581149f33ec801766bc8f7777d13471726828
hfcorriez <119550+hfcorriez@users.noreply.github.com>
2021-10-18 23:20:31 +0800

    support default service

--- functions/services/utils/getServiceConfig.js
+++ functions/services/utils/getServiceConfig.js
@@ -1,13 +1,16 @@
-module.exports = ({ args, service }, { fly }) => {
-  if (!fly.service) {
-    throw new Error('service not config in fly.yml')
-  }
+const DEFAULT_SERVICES = {
+  http: { fn: '$http' },
+  cron: { fn: '$cron', singleton: true }
+}
 
-  if (!fly.service[service]) {
+module.exports = ({ args, service }, { fly }) => {
+  const services = { ...DEFAULT_SERVICES, ...fly.service }
+
+  if (!services[service]) {
     throw new Error(`service "${service}" not found in fly.yml`)
   }
 
-  const config = { ...fly.service[service], service, ...args }
+  const config = { ...services[service], service, ...args }
   if (!config.fn) {
     throw new Error(`service "${service}" need "fn" to run`)
   }
