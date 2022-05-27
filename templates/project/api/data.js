12062b460321df00b34cbd28f0d64b6d4fffa547
hfcorriez <119550+hfcorriez@users.noreply.github.com>
2022-05-27 19:13:14 -0400

    simple context require

--- templates/project/api/data.js
+++ templates/project/api/data.js
@@ -29,9 +29,9 @@ module.exports = {
   // Main
   async main (event, {
     eventId, user, fly, handleHttp,
-    '@lib/db': db,
-    '@dayjs': dayjs,
-    '@package.json': pkg
+    'lib/db': db,
+    dayjs,
+    'package.json': pkg
   }) {
     fly.info('execute data:main')
     return {
