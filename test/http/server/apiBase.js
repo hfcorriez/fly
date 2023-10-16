37e13c243874bc4be3581c8edd5e5e508bd44b28
hfcorriez <119550+hfcorriez@users.noreply.github.com>
2023-10-16 16:16:16 -0400

    remove unused

--- test/http/server/apiBase.js
+++ test/http/server/apiBase.js
@@ -6,15 +6,14 @@ module.exports = {
 
   afterHttp (event) {
     return {
-      body: { code: 0, data: event }
+      body: { code: 0, data: event },
     }
   },
 
   catchHttp (err) {
     console.log(err)
     return {
-      body: { code: 1, message: err ? err.message : 'unknown error', stack: err.stack }
+      body: { code: 1, message: err ? err.message : 'unknown error', stack: err.stack },
     }
-  }
-
+  },
 }
